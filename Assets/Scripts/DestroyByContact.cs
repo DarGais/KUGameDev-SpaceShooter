4aaee87b02cd15f07c35e3c7015ed54cc90fcdec
DarGais <dargais_zzz@hotmail.com>
2017-02-21 21:10:07 +0700

    Add file
    Add Unity Project

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour{
+
+	public int scoreValue;
+	public int lifeMinus;
+	private GameController gameController;
+	//private int hp;
+
+	void Start(){
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController> ();
+			//hp = gameController.MaxLife;
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") {
+			return;
+		}
+
+		if (other.tag == "Player") {
+			gameController.LifeMinus (lifeMinus);
+			//hp -= lifeMinus;
+			if (gameController.MaxLife == 0) {				
+				Destroy (other.gameObject);
+				gameController.GameOver ();
+			}
+		}
+
+		gameController.AddScore (scoreValue);
+		Destroy (gameObject);
+
+
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour{
+
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject enemy3;
+	public GameObject[] enemy;
+
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	public int MaxLife;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	public GUIText lifeText;
+
+	private int score;
+	private int n;
+	private bool gameOver;
+	private bool restart;
+	//private int hp;
+	private bool death;
+
+	void Start (){
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		//hp = MaxLife;
+		lifeText.text = "Life: " + MaxLife;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update(){
+		
+		if (restart) {
+			if (Input.GetKeyDown(KeyCode.R)) {
+				UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+				//Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves(){
+		yield return new WaitForSeconds(startWait);
+		enemy = new GameObject[3];
+		enemy [0] = enemy1;
+		enemy [1] = enemy2;
+		enemy [2] = enemy3;
+
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				int n = Random.Range (0,3);
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (enemy[n],spawnPosition,spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+
+			if (gameOver) {
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore(int newScoreValue){
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore(){
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver(){
+		gameOverText.text = "Game Over!!";
+		gameOver = true;
+	}
+
+	public void LifeMinus(int minus){
+		MaxLife -= minus;
+		lifeText.text = "Life: " + MaxLife;
+	}
+			
+}
